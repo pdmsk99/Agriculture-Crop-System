21bd045ace6c9f1eed5dacfd422dd2252b8eae50
pdmsk99 <47064741+pdmsk99@users.noreply.github.com>
2021-06-15 20:43:36 +0530

    Day6
    updated the eureka server connections

--- order/src/main/java/com/example/order/order/OrderApplication.java
+++ order/src/main/java/com/example/order/order/OrderApplication.java
@@ -1,9 +1,6 @@
 package com.example.order.order;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -16,26 +13,10 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 	    HibernateJpaAutoConfiguration.class })
 public class OrderApplication{
 	
-//	@Autowired
-//	private Repository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
 	}
 	
-//	@Override
-//	public void run(String... args) throws Exception {
-//		Orders o1 = new Orders( 141615 , "SaiKrishna" , 121 , "****************");
-//		Orders o2 = new Orders( 141616 , "KrishnaSai" , 122 , "*********");
-//		
-//		repository.save(o1);
-//		repository.save(o2);
-//		System.out.println("****************");
-//		
-//		List<Orders> orders = repository.findAll();
-//		
-//		for (Orders o : orders) {
-//			System.out.println(o.toString());
-//		}
-//	}
+
 
 }
